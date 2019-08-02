88a14672ae2269a84b5a876669820d91e85f4136
chenyongk <251743041@qq.com>
2019-08-02 16:53:04 +0800

    svelte

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	export let name;
+	import Shop from './shop.svelte';
+	import { onMount } from 'svelte'
+    let arr = []
+    onMount(async function(){
+        let res = await fetch('http://localhost:7001/shop');
+        let result = await res.json();
+		arr = result.list;   
+    })
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+<h1>Hello {name}!</h1>
+<Shop {arr}/>
+
---
+++ src/shop.svelte
@@ -0,0 +1,38 @@
+<script>
+    export let arr;
+    let countTot = 0;
+    let priceTot = 0
+    function add(item) {
+        item.count++;
+        arr = arr;
+        countTot = arr.reduce((prev,next) => {
+            return prev + next.count
+        },0);
+        priceTot = arr.reduce((prev,next) => {
+            return prev + next.count*next.price
+        },0);
+    }
+</script>
+<style>
+dl{
+    display: flex
+}
+dd{
+    display:flex;
+    flex-direction: column
+}
+</style>
+总数：{countTot} <br />
+总价：{priceTot}
+{#each arr as item}
+    <dl>  
+        <dt>
+            <img src={item.img} alt="">
+        </dt>
+        <dd>
+            <span>{item.title}</span>
+            <span>价格：{item.price}</span>
+            <p><button>-</button><span>{item.count}</span><button on:click={() => add(item)}>+</button></p>
+        </dd>  
+    </dl>
+{/each}
